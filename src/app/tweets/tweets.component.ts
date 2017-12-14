6ba6a010ed43832a80b95d4e055fd9aeedcadb75
Martin Pultz <mtpultz@gmail.com>
2017-12-14 15:31:47 -0700

    Updated tweets to be in a list-group instead of a table

--- src/app/tweets/tweets.component.ts
+++ src/app/tweets/tweets.component.ts
@@ -14,26 +14,38 @@ export class TweetsComponent implements OnInit, AfterViewInit {
 
   public tweets$: Observable<any>;
 
-  constructor(public tweetService: TweetsService) {
+  constructor(
+    private tweetService: TweetsService
+  ) {
     this.tweets$ = tweetService.getTweets();
+  }
+
+  /**
+   * Outputs date in time ago format.
+   * @param {string} time
+   * @return {string}
+   */
+  // TODO: depending on reuse turn into a pipe or pull in package containing a pipe
+  public timeAgo(time) {
+    return time.fromNow();
   }
 
   ngOnInit() {
   }
 
-  ngAfterViewInit () {
-    !function(d,s,id){
-        var js: any,
-            fjs=d.getElementsByTagName(s)[0],
-            p='https';
-        if(!d.getElementById(id)){
-            js=d.createElement(s);
-            js.id=id;
-            js.src=p+"://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js,fjs);
-        }
+  ngAfterViewInit() {
+    !function (d, s, id) {
+      var js: any,
+        fjs = d.getElementsByTagName(s)[0],
+        p = 'https';
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
     }
-    (document,"script","twitter-wjs");
-}
+      (document, "script", "twitter-wjs");
+  }
 
 }
