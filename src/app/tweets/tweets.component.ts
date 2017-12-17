ce73015d99eab63e0433a6616f73131e0e1dbe12
Muhammad Fawwaz <fawwaz@builtforfifty.com>
2017-12-17 08:50:22 +0800

    Import and add ng2tweet service into constructor for tweets component

--- src/app/tweets/tweets.component.ts
+++ src/app/tweets/tweets.component.ts
@@ -6,34 +6,33 @@ import { Observable } from 'rxjs/Observable';
 
 
 @Component({
-  selector: 'app-tweets',
-  templateUrl: './tweets.component.html',
-  styleUrls: ['./tweets.component.css']
+  selector: "app-tweets",
+  templateUrl: "./tweets.component.html",
+  styleUrls: ["./tweets.component.css"]
 })
 export class TweetsComponent implements OnInit, AfterViewInit {
-
   public tweets$: Observable<any>;
 
-  constructor(public tweetService: TweetsService) {
+  constructor(
+    public tweetService: TweetsService,
+    private ng2TweetService: Ng2TweetService
+  ) {
     this.tweets$ = tweetService.getTweets();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    !(function(d, s, id) {
+      var js: any,
+        fjs = d.getElementsByTagName(s)[0],
+        p = "https";
+      if (!d.getElementById(id)) {
+        js = d.createElement(s);
+        js.id = id;
+        js.src = p + "://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }
+    })(document, "script", "twitter-wjs");
   }
-
-  ngAfterViewInit () {
-    !function(d,s,id){
-        var js: any,
-            fjs=d.getElementsByTagName(s)[0],
-            p='https';
-        if(!d.getElementById(id)){
-            js=d.createElement(s);
-            js.id=id;
-            js.src=p+"://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js,fjs);
-        }
-    }
-    (document,"script","twitter-wjs");
-}
-
 }
